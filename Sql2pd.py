ca70b44eb7937b0b4a6d0e631f7e8925f0aa4249
ywdxy123 <619998073@qq.com>
2017-09-08 20:27:50 +0800

    Update Sql2pd.py

--- Sql2pd.py
+++ Sql2pd.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
-import pymssql 
 import matplotlib.pyplot as plt
-select_sql='SELECT Date,TEMP_H,TEMP_L,Days,Month FROM WeatherRecord'
+import pymssql 
+select_sql='SELECT Date,TEMP_H,TEMP_L,Days,Month FROM WeatherRecord order by Date'
 db=pymssql.connect(host='localhost',user='sa',password='xxxxxxx',database='Test',charset='utf8')
 cur=db.cursor()
 
@@ -11,3 +11,7 @@ df=pd.read_sql(select_sql,db)
 
 a=pd.DataFrame(df)
 print(a)
+df['Date'] = pd.to_datetime(df['Date'],format='%Y-%m-%d %H:%M:%S')
+a_Date=a.set_index('Date')
+a_Date['TEMP_L'].plot()
+plt.show()
