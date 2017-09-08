d6a376ac0c58ef94b0fb6b5ecde721f98fa54a00
ywdxy123 <619998073@qq.com>
2017-09-08 20:16:01 +0800

    Update create.py

--- create.py
+++ create.py
@@ -1,16 +1,16 @@
 import pymssql
-db=pymssql.connect(host='localhost',user='sa',password='ywdxy123',database='Test',charset="utf8")
+db=pymssql.connect(host='localhost',user='sa',password='xxxxxxx',database='Test',charset="utf8")
 print(db)
 cur=db.cursor()
 
 sql='''CREATE TABLE WeatherRecord(
 ID INT PRIMARY KEY IDENTITY,
 DATE VARCHAR(255) NOT NULL,
-YEAR VARCHAR(255) NOT NULL,
-MONTH VARCHAR(255) NOT NULL,
-DAYS VARCHAR(255) NOT NULL,
-TEMP_H VARCHAR(255) NOT NULL,
-TEMP_L VARCHAR(255) NOT NULL,
+YEAR INT NOT NULL,
+MONTH INT NOT NULL,
+DAYS INT NOT NULL,
+TEMP_H INT NOT NULL,
+TEMP_L INT NOT NULL,
 WIND VARCHAR(255) NOT NULL,
 WindLevel VARCHAR(255) NOT NULL,
 DEC NVARCHAR(255) NOT NULL);'''
